274edead40703f1fa56cbbc1f9e51eef1b977156
Kaitlyn Kenwell <redrield@gmail.com>
2026-04-18 09:03:36 -0400

    Add not1mm

---
+++ pkgs/appdata.nix
@@ -0,0 +1,14 @@
+{ python3Packages, lib, fetchPypi }:
+python3Packages.buildPythonPackage rec {
+  pname = "appdata";
+  version = "2.2.1";
+
+  src = fetchPypi {
+    inherit pname version; 
+    hash = "sha256-MSIkHhaJ64vjau4orzvwgP4hbjzI+rta9ngmsUmDepI=";
+  };
+
+  build-system = with python3Packages; [ setuptools ];
+
+  dependencies = [];
+}
